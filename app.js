2d2b033657e83bcc7d7d1586febd915f73ecb22d
Olajutemo Timilehin <olajutemot@gmail.com>
2024-01-24 18:34:11 -0800

    update

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 const epress = require("express");
-const app = express();
+const userRoutes = require("./controllers/userRoutes");
 const port = 8000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+// const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       console.log("origin", origin);
+//       if (!origin) {
+//         return callback(null, true);
+//       }
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       } else {
+//         return callback(new Error("blocked by cors"));
+//       }
+//     },
+//   })
+// );
+
+app.use(cors());
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello world");
+  res.send({ message: "the API is working" });
 });
-app.get("/about", (req, res) => {
-  res.status(200).send("About PAge");
-});
-app.use("/", (req, res, next) => {
-  res.status(404).send("PAge not found");
-});
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello world");
+// });
+// app.get("/about", (req, res) => {
+//   res.status(200).send("About PAge");
+// });
+// app.use("/", (req, res, next) => {
+//   res.status(404).send("PAge not found");
+// });
 
 app.listen(port, () => {
   console.log(`server is staring on port:${port}`);
